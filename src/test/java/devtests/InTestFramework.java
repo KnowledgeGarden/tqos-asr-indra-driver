d17cf70ebedd20f71a2de9fe086c5d5715910656
Jack Park <jackpark@gmail.com>
2020-09-04 16:04:25 -0700

    first commit

---
+++ src/test/java/devtests/InTestFramework.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package devtests;
+
+/**
+ * @author jackpark
+ *
+ */
+public class InTestFramework {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new BootTest();
+
+	}
+
+}
---
+++ src/test/java/devtests/TestRoot.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package devtests;
+
+import org.topicquests.os.asr.driver.InDriverEnvironment;
+
+/**
+ * @author jackpark
+ *
+ */
+public class TestRoot {
+	protected InDriverEnvironment environment;
+
+	/**
+	 * 
+	 */
+	public TestRoot() {
+		environment = new InDriverEnvironment();
+	}
+
+}
